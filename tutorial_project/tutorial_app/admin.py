ce02c6608f558a52a36c2b1c2a6b3acd1e6da2aa
angelfarias718 <angelfarias718@gmail.com>
2016-10-27 21:11:31 -0400

    updated_commit

--- tutorial_project/tutorial_app/admin.py
+++ tutorial_project/tutorial_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
-
 from models import Category, Page
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
 
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
--- tutorial_project/tutorial_project/urls.py
+++ tutorial_project/tutorial_project/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-<<<<<<< HEAD
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
-=======
-from django.conf import settings
-from django.conf.urls.static import static
->>>>>>> 459f27552dc1899c1423957cf32db4e7fe731399
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,10 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
 	url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
 	url(r'^', include('tutorial_app.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-	url(r'^', include('tutorial_app.urls')), # ADD THIS NEW TUPLE!
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 459f27552dc1899c1423957cf32db4e7fe731399
